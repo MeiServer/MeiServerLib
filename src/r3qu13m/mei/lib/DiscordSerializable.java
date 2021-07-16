324a8fa609d7e24ea588a2f48659316e284a967e
r3qu13m <r3qu13m.minecraft@gmail.com>
2021-07-16 22:57:56 +0900

    Use deflate compression when serialize/unserialize and implement serializeDataset

--- src/r3qu13m/mei/lib/DiscordSerializable.java
+++ src/r3qu13m/mei/lib/DiscordSerializable.java
@@ -5,12 +5,20 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterInputStream;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.InflaterInputStream;
 
 public interface DiscordSerializable {
 	public void serialize(DataOutputStream dos) throws IOException;
@@ -19,7 +27,7 @@ public interface DiscordSerializable {
 
 	public static <T> String serialize(final T obj) {
 		final ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		final DataOutputStream dos = new DataOutputStream(bos);
+		final DataOutputStream dos = new DataOutputStream(new DeflaterOutputStream(bos));
 		try {
 			if (Serializable.class.isAssignableFrom(obj.getClass())) {
 				final ObjectOutputStream oos = new ObjectOutputStream(dos);
@@ -40,7 +48,7 @@ public interface DiscordSerializable {
 	@SuppressWarnings("unchecked")
 	public static <T> T unserialize(final String encodedData, final Class<T> cls) {
 		final ByteArrayInputStream bis = new ByteArrayInputStream(Base64.getDecoder().decode(encodedData));
-		final DataInputStream dis = new DataInputStream(bis);
+		final DataInputStream dis = new DataInputStream(new InflaterInputStream(bis));
 		T obj = null;
 		try {
 			if (Serializable.class.isAssignableFrom(cls)) {
@@ -59,5 +67,33 @@ public interface DiscordSerializable {
 			throw new RuntimeException(e);
 		}
 		return obj;
+	}
+
+	public static void serializeDataset(Map<String, DiscordSerializable> dataset, OutputStream os) throws IOException {
+		DataOutputStream dos = new DataOutputStream(os);
+		dos.writeInt(dataset.keySet().size());
+
+		for (String key : dataset.keySet()) {
+			dos.writeUTF(key);
+			DiscordSerializable ds = dataset.get(key);
+			dos.writeUTF(ds.getClass().getName());
+			dos.writeUTF(serialize(dataset.get(key)));
+		}
+	}
+
+	public static Map<String, DiscordSerializable> unserializeDataset(InputStream is) throws IOException {
+		DataInputStream dis = new DataInputStream(is);
+		Map<String, DiscordSerializable> ret = new HashMap<>();
+		int length = dis.readInt();
+		try {
+			for (int i = 0; i < length; i++) {
+				String key = dis.readUTF();
+				Class<?> cls = Class.forName(dis.readUTF());
+				ret.put(key, (DiscordSerializable) unserialize(dis.readUTF(), cls));
+			}
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		return ret;
 	}
 }
