2986b6c2b7ebb75beed50c0b7e89f5dfe1e895e5
r3qu13m <r3qu13m.minecraft@gmail.com>
2021-07-17 01:17:00 +0900

    make public

--- src/r3qu13m/mei/lib/FileUtils.java
+++ src/r3qu13m/mei/lib/FileUtils.java
@@ -2,14 +2,24 @@ package r3qu13m.mei.lib;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
 public class FileUtils {
-	private static String computeHash(final File target) {
+	public static String computeHash(final File target) {
 		try {
-			return DigestUtils.sha1Hex(new FileInputStream(target));
+			return FileUtils.computeHash(new FileInputStream(target));
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static String computeHash(final InputStream is) {
+		try {
+			return DigestUtils.sha1Hex(is);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
